9dd3d1d88f0e426b504ac894a8a0bc3aff7dbc36
tukeibrian <tukeibrian5@gmail.com>
2022-12-09 12:48:54 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main -print a C  program using printf
  *
- * Returns: 0
+ * Return: 0 
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
