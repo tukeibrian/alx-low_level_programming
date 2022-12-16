e30d6e3a80ac8193486ef2beb3cc0b525d0dfa7e
tukeibrian <tukeibrian5@gmail.com>
2022-12-16 00:12:04 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * @n: the num to be checked for
  * Return: 1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
+ * Returns -1 and prints -1 if n is less than zero
 */
 
 int print_sign(int n)
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @t: The integer to be computed
+ *
+ * Return :The absolute value
+*/
+
+int _abs(int)
+{
+	if (n >= 0)
+		return (t);
+	else
+		return (-t);
+}
