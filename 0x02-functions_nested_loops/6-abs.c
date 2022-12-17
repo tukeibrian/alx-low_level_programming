65183ac9ccb8c4daf9e0deb009784349b885af4f
tukeibrian <tukeibrian5@gmail.com>
2022-12-17 13:31:41 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * _abs - computes the absolute value of an integer
  * @t: The integer to be computed
- * Return :0 The absolute value of the integer.
+ * Return: The absolute value of the integer.
 */
 
 int_abs(int t)
