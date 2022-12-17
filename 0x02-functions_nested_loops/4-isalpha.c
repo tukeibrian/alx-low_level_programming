b4b0006c15bfc4ee35e6954e73d4e4d80436744c
tukeibrian <tukeibrian5@gmail.com>
2022-12-17 14:52:43 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  *
  * Return: 1 if character is aletter, lowercase or uppercase, 0 otherwise.
 */
-int _isalpha(int c)
+int_isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') ||
 			(c >= 'A' && c <= 'Z'))
