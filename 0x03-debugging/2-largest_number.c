e4bbd364f710df3861396357315bec3810018769
tukeibrian <tukeibrian5@gmail.com>
2022-12-19 01:34:42 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > c)
+	else if (b > a && a > c)
+	{
 		largest = b;
-}
-else
-}
-largest = c;
-}
+	}
+	else
+	{
+		largest = c;
+	}
 
-return (largest);
+	return (largest);
 }
