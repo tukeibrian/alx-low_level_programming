690f7ac8af3d9a1800b872066c6fa2b72cde1cfb
tukeibrian <tukeibrian5@gmail.com>
2022-12-21 07:32:51 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
-int _putchar.c(char c);
+int _strlen(char *s);;
 
 #endif
