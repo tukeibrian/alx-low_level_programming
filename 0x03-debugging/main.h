7c9086530b29747401caef6b548ba16b55f003c6
tukeibrian <tukeibrian5@gmail.com>
2022-12-21 12:11:12 -0800

    last task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,6 @@ fndef MAIN_H
 void positive_or_negative(int i);
 int main(void);
 int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
+int convert_day(int month, int day, int year);
 
 #endif
