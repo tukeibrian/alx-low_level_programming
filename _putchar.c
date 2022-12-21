b50ea8cf6a0e2ba1cecf2726677c10da3cfb55f8
tukeibrian <tukeibrian5@gmail.com>
2022-12-20 23:36:02 -0800

    0-reset_to_98.c

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes character c to stdout
+ * @c: character to be printed
+ * Return: on success 1
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
