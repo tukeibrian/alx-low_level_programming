8fbaab6bde8d0db3a72a5d8e9091d7532809dc59
tukeibrian <tukeibrian5@gmail.com>
2022-12-26 11:45:53 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -17,11 +17,11 @@ char *rot13(char *s)
 		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < s2; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
-				s[i] - datarot[j];
+				if s[i] - datarot[j];
 				break;
 			}
 		}
