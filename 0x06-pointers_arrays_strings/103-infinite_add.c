a153a499e24a010c02e79017bea48b2bbc49ac95
tukeibrian <tukeibrian5@gmail.com>
2022-12-28 23:45:00 -0800

    task11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,7 +11,12 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int i;
+	int j;
+	int k;
+	int l;
+	int m;
+	int n;
 
 	for (i = 0; n1[i]; i++);
 	for (j = 0; n2[j]; j++);
