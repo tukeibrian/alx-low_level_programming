b03f4ab801ff2e96c203ce945f0c976f4dc3017e
tukeibrian <tukeibrian5@gmail.com>
2022-12-31 07:42:45 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * main - prints arandom number and states whether
  *        it is positive, negative, or zero.
  *
- * Return: Always 0. 
+ * Return: Always 0
 */
 
 int main(void)
@@ -17,11 +17,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is positive\n" , n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("%d is negative\n" , n);
-	else 
-		printf("%d is zero\n" , n);
+		printf("%d is negative\n", n);
 
-	return (0)
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
 }
