81422b6c0efbec104d20108f35b7fe8506638e46
tukeibrian <tukeibrian5@gmail.com>
2023-01-02 06:00:41 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - Fills the first n bytes of the memory area
+ * @s: Apointer to the memory area to be filled
+ * @b: The character to fill the memory area with
+ * @n: The number of bytes to be filled
+ *description _memset: over there
+ * Return: Apointer to a filled memory area @s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = b;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+	return (memory);
+}
