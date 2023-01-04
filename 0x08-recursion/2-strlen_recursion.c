9bcf5e003a61c74b6a5fd969a818a799307a40aa
tukeibrian <tukeibrian5@gmail.com>
2023-01-04 05:08:46 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - length of a string.
+ * _strlen_recursion - Returns the length of a string.
  * @s: The string to be measured
  * Return: strlen_recursion
 */
